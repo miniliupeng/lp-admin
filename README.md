83b55b04e0e2bbacf7d61b3ec318610fe0e6310a
刘鹏 <2254842466@qq.com>
2024-04-12 19:46:35 +0800

    feat: 集成依赖

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
+## 项目规范
+
 pnpm install -D husky @commitlint/config-conventional @commitlint/cli
 
 npx husky-init
 添加 pnpm lint
 
 echo "npx --no-install commitlint --edit "$1"" > .husky/commit-msg
+
+## 项目插件
+
+pnpm install -D @unocss/vite @unocss/preset-uno @unocss/transformer-directives sass vite-plugin-progress vite-plugin-compression vite-plugin-mock@2.9.8 unplugin-vue-components unplugin-auto-import vite-plugin-svg-icons vite-plugin-style-import consola rollup-plugin-visualizer @types/mockjs @types/nprogress
+
+pnpm install @element-plus/icons-vue element-plus @vueuse/core axios dayjs echarts mockjs nprogress vue-i18n
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,45 @@
-import { fileURLToPath, URL } from 'node:url'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
+import { setupVitePlugins } from './build'
+import pkg from './package.json'
+import { resolve } from 'path'
+import dayjs from 'dayjs'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
+const { dependencies, devDependencies, name, version } = pkg
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss')
+}
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig((configEnv: ConfigEnv) => {
+  const viteEnv = loadEnv(configEnv.mode, process.cwd()) as ImportMetaEnv
+
+  return {
+    base: viteEnv.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      open: true,
+      https: false,
+      proxy: {}
+    },
+    plugins: setupVitePlugins(viteEnv),
+    build: {
+      sourcemap: false,
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+        }
+      }
+    },
+    define: { __APP_INFO__: JSON.stringify(__APP_INFO__) }
   }
 })
